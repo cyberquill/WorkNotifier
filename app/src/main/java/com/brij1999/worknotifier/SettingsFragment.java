dcbbd976f6d75d0841bb2e210ff6981fd487a388
Brijgopal Bharadwaj <brijgopalbharadwaj1999@gmail.com>
2022-07-25 22:29:57 +0530

    Prevented monitoring of non-clearable notifications, added option to hide WorkNotifier-Enabled notification

--- app/src/main/java/com/brij1999/worknotifier/SettingsFragment.java
+++ app/src/main/java/com/brij1999/worknotifier/SettingsFragment.java
@@ -1,5 +1,11 @@
 package com.brij1999.worknotifier;
 
+import static com.brij1999.worknotifier.WorkNotifierListenerService.SERVICE_NOTIFICATION_ID;
+import static com.brij1999.worknotifier.WorkNotifierListenerService.WORKNOTIFIER_HIDE_MONITOR_NTF;
+import static com.brij1999.worknotifier.WorkNotifierListenerService.WORKNOTIFIER_LISTENER_ACTIVE;
+
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,26 +15,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import com.brij1999.worknotifier.BuildConfig;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class SettingsFragment extends Fragment {
 
+    private TinyDB tinydb;
+    NotificationManager mNotificationManager;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-//        Logger logger = Logger.getInstance(view.getContext());
-//        Button logBtn = view.findViewById(R.id.logBtn);
-//        Button clsBtn = view.findViewById(R.id.clsBtn);
-//        TextView logView = view.findViewById(R.id.logView);
-//        logBtn.setOnClickListener((v) -> {
-//            logger.log("SettingsFragment","onCreateView","<break/>");
-//            logView.setText(logger.get());
-//        });
-//        clsBtn.setOnClickListener((v) -> {
-//            logger.clear();
-//            logView.setText("");
-//        });
+        mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        tinydb = new TinyDB(view.getContext());
+        int versionCode = BuildConfig.VERSION_CODE;
+        String versionName = BuildConfig.VERSION_NAME;
+
+        TextView appVersion = view.findViewById(R.id.appVersion);
+        appVersion.setText(versionName+"  <"+versionCode+"/>");
+        SwitchMaterial activeNtfSwitch = view.findViewById(R.id.activeNtfSwitch);
+        activeNtfSwitch.setChecked(tinydb.getBoolean(WORKNOTIFIER_HIDE_MONITOR_NTF));
+        activeNtfSwitch.setOnClickListener((v) -> {
+            boolean val = !tinydb.getBoolean(WORKNOTIFIER_HIDE_MONITOR_NTF);
+            tinydb.putBoolean(WORKNOTIFIER_HIDE_MONITOR_NTF, val);
+            activeNtfSwitch.setChecked(val);
+
+            if(tinydb.getBoolean(WORKNOTIFIER_HIDE_MONITOR_NTF)) {
+                mNotificationManager.cancel(SERVICE_NOTIFICATION_ID);
+            }
+        });
         return view;
     }
 }
